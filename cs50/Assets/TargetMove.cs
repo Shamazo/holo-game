a3464f9eabbda31e5a5f95067f65a8c6c01a0c98
hamish-PC\hamish <hamish_nicholson@college.harvard.edu>
2016-12-02 04:23:38 -0500

    balls move in circles now

---
+++ cs50/Assets/TargetMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetMove : MonoBehaviour {
+    public float speed = 30;
+    public float height = .5F;
+    private Vector3 pivot = new Vector3(0, .5F , 0);
+
+    // Use this for initialization
+    void Start() {
+
+    //public float hieght = transform.position.y;
+
+
+    }
+
+
+// Update is called once per frame
+void Update () {
+
+        transform.RotateAround(pivot, Vector3.up, speed * Time.deltaTime);
+    }
+
+
+}
