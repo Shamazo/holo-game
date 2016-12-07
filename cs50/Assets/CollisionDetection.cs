627b85d8b3cf0f53bd768f44d89a6f83ae11196b
DESKTOP-9S5ESMP\RaymondW <raymond_wang@college.harvard.edu>
2016-12-07 14:49:18 -0500

    Life is really great when things work (added score and timer)

--- cs50/Assets/CollisionDetection.cs
+++ cs50/Assets/CollisionDetection.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class CollisionDetection : MonoBehaviour {
 
     public GameObject TargetSpawner;
+    public GameObject ScoreDisplay;
     //called when the projectile and target collide
     void OnTriggerEnter(Collider other)
     {
+        ScoreDisplay = GameObject.FindGameObjectWithTag("ScoreDisplay");
         TargetSpawner = GameObject.FindGameObjectWithTag("Respawn");
         TargetSpawner.gameObject.SendMessage("delete");
+        ScoreDisplay.gameObject.SendMessage("scoreincrease");
         Debug.Log("Collision detected with trigger object " + other.name);
             //we want both the target and the projectile to be destroyed when they collide
             Destroy(gameObject);
